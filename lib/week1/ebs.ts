8cea53e0211de58e58eb0d051347c6d0043a9b67
Young Woo Jung <youngwjung@gmail.com>
2022-06-12 15:50:54 +0000

    hide bootstrap commands

--- lib/week1/ebs.ts
+++ lib/week1/ebs.ts
@@ -1,5 +1,6 @@
 import * as cdk from "aws-cdk-lib";
 import * as ec2 from "aws-cdk-lib/aws-ec2";
+import * as iam from "aws-cdk-lib/aws-iam";
 import { Construct } from "constructs";
 
 export class EbsStack extends cdk.Stack {
@@ -21,10 +22,19 @@ export class EbsStack extends cdk.Stack {
       ],
     });
 
+    const role = new iam.Role(this, "Role", {
+      roleName: "week1-ebs-role",
+      assumedBy: new iam.ServicePrincipal("ec2.amazonaws.com"),
+    });
+
+    role.addManagedPolicy(
+      iam.ManagedPolicy.fromAwsManagedPolicyName("AmazonSSMManagedInstanceCore")
+    );
+
     const user_data = ec2.UserData.forLinux();
-    user_data.addCommands("> /home/ec2-user/.ssh/authorized_keys");
     user_data.addCommands(
-      "echo SGFwcHkgbmV3IHllYXIh | base64 -d > /home/ec2-user/secret.txt"
+      "aws s3 cp s3://youngwjung/awslabs/week1-ebs.sh /home/ec2-user/",
+      "bash /home/ec2-user/week1-ebs.sh"
     );
 
     const instance = new ec2.Instance(this, "instance", {
@@ -38,6 +48,7 @@ export class EbsStack extends cdk.Stack {
       }),
       userData: user_data,
       keyName: keypair.valueAsString,
+      role: role,
     });
   }
 }
