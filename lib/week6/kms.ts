f1dc4726c930ec2798d7669b28a58aca2c40fb16
youngwjung <youngwjung@gmail.com>
2022-08-17 10:06:58 +0900

    update rds mysql minor version

--- lib/week6/kms.ts
+++ lib/week6/kms.ts
@@ -44,7 +44,7 @@ export class KmsStack extends cdk.Stack {
 
     const mysql = new rds.DatabaseInstance(this, "mysql", {
       engine: rds.DatabaseInstanceEngine.mysql({
-        version: rds.MysqlEngineVersion.VER_8_0_21,
+        version: rds.MysqlEngineVersion.VER_8_0_28,
       }),
       vpc: vpc,
       instanceType: ec2.InstanceType.of(
